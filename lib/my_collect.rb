a9c49d62fbe601704e57f4115229427846781855
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-18 22:53:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  new_arr = []
+  while counter < collection.length
+    new_arr << yield(collection[counter])
+    counter += 1
+  end
+  return new_arr
+end
